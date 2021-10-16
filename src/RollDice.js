0b885e04a02301e69c4eb089ebfc48dcc5e44793
Malte Hoch <mhoch94@gmail.com>
2021-10-16 11:22:31 +0200

    Added Win, Attempts & Button disabling

--- src/RollDice.js
+++ src/RollDice.js
@@ -7,7 +7,14 @@ import Dice from './Dice';
 class RollDice extends React.Component {
   state = {
     dice1: 'one',
-    dice2: 'two'
+    dice2: 'two',
+  }
+
+  constructor(props) {
+    super(props);
+    this.attempts = 0;
+    this.victory = "";
+    this.btnDisabled = false;
   }
 
   static defaultProps = {
@@ -19,17 +26,34 @@ class RollDice extends React.Component {
     const Diced2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     this.setState({
         dice1: Diced1,
-        dice2: Diced2
+        dice2: Diced2,
     })
+
+    if (this.state.dice1 === "six" && this.state.dice2 === "six") {
+      this.victory = <p style={{color: 'green', fontWeight: '600'}}>You win!</p>;
+      this.btnDisabled = true;
+    }
+
+  }
+
+  incAttempts = () => {
+    this.attempts = this.attempts + 1;
   }
 
   render() {
       return (
         <div className="RollDice">
-          <Dice face={this.state.dice1} />
-          <Dice face={this.state.dice2} />
-          <span className="flex-break" />
-          <button onClick={this.roll}>Roll Dice</button>
+          <div className="flex">
+            <Dice face={this.state.dice1} />
+            <Dice face={this.state.dice2} />
+            <span className="flex-break" />
+            <button disabled={this.btnDisabled} onClick={() => { this.roll(); this.incAttempts(); }}>Roll Dice</button>
+            <span className="flex-break" />
+            <br />
+            Attempts: {this.attempts}
+            <span className="flex-break" />
+            {this.victory}
+          </div>
         </div>
     )
   }
